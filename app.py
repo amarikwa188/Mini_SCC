13ae3cdcf99edb1aec38968bf863c347568dab7d
Amarikwa <amarikwa188@gmail.com>
2024-07-31 12:13:44 +0100

    update ui based on status value

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app: Flask = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["post", "get"])
 def index() -> str:
-    return render_template("index.html")
+    if request.method == "POST":
+        url: str = request.form["url"]
+        print(f"\n{url}\n")
+
+    is_online: bool = True 
+    return render_template("index.html", status=is_online)
 
 
 if __name__ == "__main__":
